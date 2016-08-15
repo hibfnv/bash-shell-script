f6f5df3c0ee803e5f2ae9283b1f35829f8d9665d
Eason Tsui <hibfnv@hotmail.com>
2016-08-15 12:25:40 +0800

    Update interactive_create_links.sh

--- interactive_create_links.sh
+++ interactive_create_links.sh
@@ -3,11 +3,25 @@
 # get user input
 echo -n "Please enter the filename your want to create links: "
 read name
-# get path for file
-# waiting for check next day
-if [-f $name ]
-   then
-      ln -s $name destination_path/$name
+echo
+echo -ne "\033[35mEnter the path of the $name: \033[0m"
+read path
+
+# Find the path first, then check file name.
+
+if [ -e $path ]
+then
+echo -ne "\033[35mGreat, $path founded.\033[0m"
 else
-  echo "Sorry, the filename you input was not found."
-  exit 0
+echo -ne "\033[35mThere's no $path.\033[0m"
+fi
+
+# File name checked in this section
+
+if [ -f name ]
+then
+echo -ne "\033[35m$name was found.\033[0m"
+else
+echo -ne "\033[35mSorry, I can't find $name.\033[0m"
+      exit 0
+fi
